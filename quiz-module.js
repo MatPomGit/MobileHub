91d917540fcea59dd376087e3171f7fbe21e2e92
Vidar <33669232+MatPomGit@users.noreply.github.com>
2026-05-27 10:56:05 +0200

    Update quiz-module.js

--- quiz-module.js
+++ quiz-module.js
@@ -1,10 +1,7 @@
 'use strict';
 
-// Witajcie studenci! tak, to tutaj jest kod dotyczący modułu testów.
-// Zapraszam do czytania i zrozumienia jak i co dziala.
-
-const QUIZ_QUESTIONS_URL = 'quiz-questions.json';
-const LETTERS = ['A', 'B', 'C', 'D'];
+const QUIZ_QUESTIONS_URL = 'quiz-questions.json'; // Witajcie studenci! tak, to tutaj jest kod dotyczący modułu testów.
+const LETTERS = ['A', 'B', 'C', 'D']; // Zapraszam do czytania i zrozumienia jak i co dziala.
 const QUIZ_MODE_SIZES = {
     // Mapa trybów testu na docelową liczbę pytań w sesji.
     short: 25,
